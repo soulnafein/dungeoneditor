df50eaf8b12544e5036d85d2f299e0756281ad7c
David S <soulnafein@gmail.com>
2011-09-18 11:54:31 +0100

    First commit

---
+++ website/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dungeoneditor::Application.config.secret_token = '6514b0f9237853d1aa81ccf9bcbd16daa46c463e5d5e7bf46c8bff89dadecc531bc7c4d4339cef158a9ba9ae6750660db79fabc25ebef89f7b40e5a29d222d83'
